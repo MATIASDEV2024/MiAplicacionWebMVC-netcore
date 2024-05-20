d3d4545b9bce92f53aaa6498a7f2ae440c337ac3
leon2 <leonu290@gmail.com>
2024-05-20 12:44:38 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiAplicacionWebMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# MiAplicacionWebMVC-netcore
